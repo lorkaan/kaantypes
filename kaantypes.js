46c54ac44ecd2626ec810546451b44bb50d520b5
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2022-11-26 17:02:58 -0500

    Update kaantypes.js

--- kaantypes.js
+++ kaantypes.js
@@ -12,7 +12,21 @@
   *
   * This is licensed under the Creative Commons License.
   *
-  * < Actual License stuff is to be put here >
+  * 
+          DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+  
   *
   */
 
